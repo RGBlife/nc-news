0800594013afca861538632b1ea76c51f71d4fb9
Bret <bret_gomes811@hotmail.co.uk>
2023-10-27 13:37:36 +0100

    adjustment task 10: Sort articles
    - feedback on Nav buttons for future features that have not yet been implemented

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,25 +1,35 @@
-import TopicsDropDown from "../components/TopicsDropDown"
+import TopicsDropDown from "../components/TopicsDropDown";
 
 const Nav = () => {
-    return (
-        <nav className="flex justify-center items-center">
-        <ul className="flex gap-5">
-          <TopicsDropDown/>
-          <li className="flex flex-1">
-            <a className=" text-[20px] text-[#FFFFFF] font-semibold hover:text-[#50192b]">
-              <button className="flex justify-center items-center leading-3 bg-[#D83367] rounded-lg h-[35px] w-[35px] hover:bg-[#C02759] transition delay-75">
-                +
-              </button>
-            </a>
-          </li>
-          <li className="flex items-center">
-            <a className=" text-[20px] text-[#D83367] font-semibold hover:text-[#50192b] transition delay-75">
-              User
-            </a>
-          </li>
-        </ul>
-      </nav>
-    )
-}
+  return (
+    <nav className="flex justify-center items-center">
+      <ul className="flex gap-5">
+        <TopicsDropDown />
+        <li className="flex flex-1">
+          <a className=" text-[20px] text-[#FFFFFF] font-semibold hover:text-[#50192b]">
+            <button
+              onClick={() => {
+                alert("'Add an article / Add a Topic' feature coming soon");
+              }}
+              className="flex justify-center items-center leading-3 bg-[#D83367] rounded-lg h-[35px] w-[35px] hover:bg-[#C02759] transition delay-75"
+            >
+              +
+            </button>
+          </a>
+        </li>
+        <li className="flex items-center">
+          <a
+            onClick={() => {
+              alert("'User profile' feature coming soon");
+            }}
+            className=" text-[20px] text-[#D83367] font-semibold hover:text-[#50192b] transition delay-75"
+          >
+            User
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
