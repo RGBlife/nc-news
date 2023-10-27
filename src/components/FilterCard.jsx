1b82d751db5e567250b96729b52636dd7be0856f
Bret <bret_gomes811@hotmail.co.uk>
2023-10-27 13:33:10 +0100

    adjustment task 10: Sort articles
    - Styling FilterCard component

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -6,19 +6,57 @@ const FilterCard = ({ setSearchParams, searchParams }) => {
   };
 
   return (
-    <div>
-      <select className="text-[20px] text-[#D83367] font-semibold hover:text-[#50192b] transition delay-75 appearance-none bg-transparent border-none leading-tight focus:outline-none focus:shadow-outline" defaultValue="" aria-label="Filter Type" onChange={(event) => handleChange("sortBy", event.target.value)}>
-        <option disabled value="">Choose a filter</option>
+    <div className="flex items-center gap-5">
+      <select
+        className="text-[20px] text-[#D83367] font-semibold hover:text-[#50192b] transition delay-75 appearance-none bg-transparent border-none leading-tight focus:outline-none focus:shadow-outline"
+        defaultValue=""
+        aria-label="Filter Type"
+        onChange={(event) => handleChange("sortBy", event.target.value)}
+      >
+        <option disabled value="">
+          Choose a filter
+        </option>
         <option value="created_at">Date</option>
         <option value="comments">Comment Count</option>
         <option value="votes">Votes</option>
       </select>
+      <svg
+        className="text-[20px] text-[#D83367] group-hover:text-[#50192b] font-semibold transition delay-75 appearance-none bg-transparent border-none leading-tight focus:outline-none focus:shadow-outline"
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"
+        />
+      </svg>
 
-      <select className="text-[20px] text-[#D83367] font-semibold hover:text-[#50192b] transition delay-75 appearance-none bg-transparent border-none leading-tight focus:outline-none focus:shadow-outline" defaultValue="" aria-label="Order Type" onChange={(event) => handleChange("order", event.target.value)}>
-        <option disabled value="">Order</option>
+      <select
+        className="text-[20px] text-[#D83367] font-semibold hover:text-[#50192b] transition delay-75 appearance-none bg-transparent border-none leading-tight focus:outline-none focus:shadow-outline"
+        defaultValue=""
+        aria-label="Order Type"
+        onChange={(event) => handleChange("order", event.target.value)}
+      >
+        <option disabled value="">
+          Order
+        </option>
         <option value="asc">Ascending</option>
         <option value="desc">Descending</option>
       </select>
+      <svg
+        className="text-[20px] text-[#D83367] group-hover:text-[#50192b] font-semibold transition delay-75 appearance-none bg-transparent border-none leading-tight focus:outline-none focus:shadow-outline"
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"
+        />
+      </svg>
     </div>
   );
 };
